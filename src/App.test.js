e9ecf4b7609482ce129710eb2667101f5e305661
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2021-03-07 13:14:08 -0500

    moved all tests to App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,68 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Todo from "./Todo"
+import TodoList from "./TodoList"
+import App from "./App"
 
-test('renders successfully', () => {
+// Smoke tests
+it("renders Todo without crashing", function () {
+  render(<Todo />);
+});
+
+it("renders TodoList without crashing", function () {
+  render(<TodoList />);
+});
+
+it("renders App without crashing", function () {
   render(<App />);
+});
+
+
+// Snapshots
+it("matches snapshot", function () {
+  const { asFragment } = render(<Todo />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+it("matches snapshot", function () {
+  const { asFragment } = render(<TodoList />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+it("matches snapshot", function () {
+  const { asFragment } = render(<App />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+
+
+// Todo functionality
+it ("should render with one todo", () => {
+    const {getByText} = render(<App />);
+    const firstTodo = getByText("laundry")
+    expect (firstTodo).toBeInTheDocument();
+});
+
+it ("should successfully add todo", () => {
+    const {getByText, queryByText, getByLabelText} = render(<App />);
+
+    const doesntShowTodo = queryByText("dishes")
+    expect(doesntShowTodo).not.toBeInTheDocument();
+
+    const input = getByLabelText("Task")
+    fireEvent.change(input, { target: { value: 'dishes' } })
+    const button = getByText("Add");
+    fireEvent.click(button);
+    const newTodo = getByText("dishes")
+    expect(newTodo).toBeInTheDocument();
+
+});
+
+it ("should successfully remove todo", () => {
+    const {getByText} = render(<App />);
+    const firstTodo = getByText("laundry")
+    expect (firstTodo).toBeInTheDocument();
+    const removeBtn = getByText("X");
+    fireEvent.click(removeBtn);
+    expect (firstTodo).not.toBeInTheDocument();
 });
